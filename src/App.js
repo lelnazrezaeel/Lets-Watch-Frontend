0d0a20a318cf282fc13e9e2560f4107224f36c3f
Sahand Nazarzadeh <Sahand.Nazarzadeh@gmail.com>
2023-04-22 00:08:46 +0330

    chore(version): Change app.js and set it version to 0.0.1

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-// Theme
-import { ThemeProvider } from '@mui/material/styles'
-import { theme } from './Theme/ThemeGenerator'
-import CssBaseline from '@mui/material/CssBaseline'
-//
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import {React} from 'react'
+// // import { BrowserRouter } from 'react-router-dom'
+// import { ThemeProvider } from '@mui/material/styles'
+// import { theme } from './Theme/ThemeGenerator'
+// import CssBaseline from '@mui/material/CssBaseline'
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <React.Fragment>
-        <CssBaseline />
-        <BrowserRouter>
-          <div>
-            <Routes>
-              //Routes
-              //<Route path='/Login' element={<Login/>} />
-            </Routes>
-          </div>
-        </BrowserRouter>
-      </React.Fragment>
-    </ThemeProvider>
+  <p>Version 0.0.1 of front-end</p>
   );
 }
 
