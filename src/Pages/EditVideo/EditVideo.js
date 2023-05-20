a85a4164b599553b61ad287cfb251f7604eb16f6
lhoorie <hoorieh95@gmail.com>
2023-05-20 13:32:16 +0330

    build(edit-video): Init edit-video page

--- src/Pages/EditVideo/EditVideo.js
+++ src/Pages/EditVideo/EditVideo.js
@@ -1,8 +1,62 @@
 import React from 'react'
 import './EditVideo.css'
-import { Typography, Paper, Box } from '@mui/material';
+import { Typography, Paper, Box, TextField } from '@mui/material';
+import { Theme, useTheme } from "@mui/material/styles";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import Chip from "@mui/material/Chip";
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const names = [
+  'Oliver Hansen',
+  'Van Henry',
+  'April Tucker',
+  'Ralph Hubbard',
+  'Omar Alexander',
+  'Carlos Abbott',
+  'Miriam Wagner',
+  'Bradley Wilkerson',
+  'Virginia Andrews',
+  'Kelly Snyder',
+];
+
+function getStyles(name, personName, theme) {
+  return {
+    fontWeight:
+      personName.indexOf(name) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
+}
+
 
 function EditVideo() {
+  const theme = useTheme();
+  const [personName, setPersonName] = React.useState([]);
+
+  const handleChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setPersonName(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value,
+    );
+  };
+
   return (
     <div>
       <Box
@@ -13,15 +67,63 @@ function EditVideo() {
           "& > :not(style)": {
             m: 1,
             mt: 5,
-            width: '60vw',
-            height: '90vh',
+            width: "60vw",
+            height: "90vh",
           },
         }}
       >
-        <Paper sx={{borderRadius: '20px'}} elevation={3}>
-            <Typography>
-                Edit Video
-            </Typography>
+        <Paper
+          sx={{ borderRadius: "20px", backgroundColor: "#c4d4f4" }}
+          elevation={3}
+        >
+          <Typography
+            variant="h1"
+            component="h2"
+            fontWeight="bold"
+            color="#1b1858"
+            sx={{ m: 4 }}
+          >
+            Edit Video
+          </Typography>
+          <TextField
+            className="name-textfield"
+            sx={{ m: 4 }}
+            required
+            id="filled-required"
+            label="Video Name"
+            defaultValue=""
+            variant="filled"
+          />
+
+          <FormControl sx={{ m: 4, width: 300 }}>
+            <InputLabel id="demo-multiple-chip-label">Tags</InputLabel>
+            <Select
+              labelId="demo-multiple-chip-label"
+              id="demo-multiple-chip"
+              multiple
+              value={personName}
+              onChange={handleChange}
+              input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
+              renderValue={(selected) => (
+                <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
+                  {selected.map((value) => (
+                    <Chip key={value} label={value} />
+                  ))}
+                </Box>
+              )}
+              MenuProps={MenuProps}
+            >
+              {names.map((name) => (
+                <MenuItem
+                  key={name}
+                  value={name}
+                  style={getStyles(name, personName, theme)}
+                >
+                  {name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
         </Paper>
       </Box>
     </div>
